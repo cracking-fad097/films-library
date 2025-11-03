e87d2e602324cb48dd51c23ad951f0f9b5bb222c
cracking-fad097 <crackingfad@gmail.com>
2025-11-03 19:58:09 +0100

    Update

--- src/App.jsx
+++ src/App.jsx
@@ -1,34 +1,28 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
+import { Routes, Route } from "react-router-dom"
+import { Layout } from "./components/Layout/Layout.jsx"
+import { Home } from "./pages/Home/Home.jsx"
+import { FilmDetails } from "./pages/FilmDetails/FilmDetails.jsx"
+import { Movie } from "./pages/Movie/Movie.jsx"
+import { Favourite } from "./pages/Favourite/Favourite.jsx"
+import { Cast } from "./pages/Cast/Cast.jsx"
+import { Review } from "./pages/Review/Review.jsx"
+import { Trailer } from "./pages/Trailer/Trailer.jsx"
 
 function App() {
-  const [count, setCount] = useState(0)
 
   return (
-    <>
-      <div>
-        <a href="https://vite.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.jsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
-    </>
+      <Routes>
+        <Route path="/" element={<Layout />}>
+          <Route path="/" element={<Home />}/>
+          <Route path="/FilmDetails" element={<FilmDetails />}>
+            <Route path="Cast" element={<Cast />}/>
+            <Route path="Review" element={<Review />}/>
+            <Route path="Trailer" element={<Trailer />}/>
+          </Route>
+          <Route path="/Movie" element={<Movie />}/>
+          <Route path="/Favourite" element={<Favourite />}/>
+        </Route>
+      </Routes>
   )
 }
 
---
+++ src/pages/Movie/Movie.jsx
@@ -0,0 +1,5 @@
+export const Movie = () => {
+    return <>
+        Movie
+    </>
+}
